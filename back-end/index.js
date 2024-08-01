406f253a073ffa3da9e6ede442d266ea73dc0e73
ayoub <ayoubkoko610@gmail.com>
2024-08-01 13:00:53 +0100

    upload backend

--- back-end/index.js
+++ back-end/index.js
@@ -11,6 +11,7 @@ const corsConfig = {
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"] 
 };
+app.options("",cors(corsConfig))
 app.use(cors(corsConfig))
 const bcrypt = require('bcrypt');
 const UserModel = require('./model/UsersModel'); // Ensure the model file path is correct
